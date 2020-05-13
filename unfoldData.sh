fb24d683b19c1ec902321636bec82bc346bd1efe
Tomas Snellman <towisnel@gmail.com>
2020-05-13 23:49:54 +0300

    Change input file names

--- unfoldData.sh
+++ unfoldData.sh
@@ -1,12 +1,12 @@
 #/usr/bin/sh
 python Unfolding.py \
-    legotrain_512_20180523-2331_LHCb4_fix_CF_pPb_MC_ptHardMerged.root \
-    legotrain_512_20180523-2331_LHCb4_fix_CF_pPb_MC_ptHardMerged.root\
-    legotrain_CF_pPb_1839_20180613_LHC13bcde.root \
+    legotrain_610_20181010-1926_LHCb4_fix_CF_pPb_MC_ptHardMerged.root \
+    legotrain_610_20181010-1926_LHCb4_fix_CF_pPb_MC_ptHardMerged.root \
+    legotrain_CF_pPb_2305_20190109_LHC13bcde.root \
     MB
 
 python Unfolding.py \
-    legotrain_512_20180523-2331_LHCb4_fix_CF_pPb_MC_ptHardMerged.root \
-    legotrain_512_20180523-2331_LHCb4_fix_CF_pPb_MC_ptHardMerged.root \
-    legotrain_CF_pPb_1839_20180613_LHC13bcde.root \
+    legotrain_610_20181010-1926_LHCb4_fix_CF_pPb_MC_ptHardMerged.root \
+    legotrain_610_20181010-1926_LHCb4_fix_CF_pPb_MC_ptHardMerged.root \
+    legotrain_CF_pPb_2305_20190109_LHC13bcde.root \
     Triggered
